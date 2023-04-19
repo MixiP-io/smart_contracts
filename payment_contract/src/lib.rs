52f2851d9824e293e538a14989af413e7295f6ca
David Roldán <62166813+Odraxs@users.noreply.github.com>
2023-04-19 14:40:26 -0500

    Implement the initialization function (#17)
    * Add initialize implementation
    
    * Add creator address to initialize

--- payment_contract/src/lib.rs
+++ payment_contract/src/lib.rs
@@ -1,10 +1,24 @@
 #![no_std]
 
+mod error;
 mod payment_contract_info;
+mod storage_types;
 
-use soroban_sdk::contractimpl;
+use error::ContractError;
+use payment_contract_info::{has_contact_info, PaymentContractInfo};
+use soroban_sdk::{contractimpl, panic_with_error, Address, Env};
 
 pub struct PaymentContract;
 
 #[contractimpl]
-impl PaymentContract {}
+impl PaymentContract {
+    pub fn initialize(env: Env, contract_info: PaymentContractInfo, creator: Address) {
+        if has_contact_info(&env) {
+            panic_with_error!(env, ContractError::AlreadyInitialized);
+        }
+        payment_contract_info::write_contract_info(&env, &contract_info);
+        payment_contract_info::write_creator(&env, &creator)
+    }
+}
+
+mod test;
---
+++ payment_contract/src/storage_types.rs
@@ -0,0 +1,13 @@
+//! Module StorageTypes
+//!
+//! Module that defines the set of keys that can be used to access and store data within the contract.
+use soroban_sdk::contracttype;
+
+#[derive(Clone)]
+#[contracttype]
+pub enum DataKey {
+    /// Stores all the metadata of the contract as a `PaymentContractInfo` struct
+    PaymentContractInfo,
+    /// To store the partner that is requested to accept the contract
+    AuthorizedPartner,
+}
