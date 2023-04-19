db95e0e8b8c66f58f3c7ca65343400a3e302aee7
David Roldán <deroldan98@gmail.com>
2023-04-19 11:36:20 -0500

    Add additional fields

--- payment_contract/src/payment_contract_info.rs
+++ payment_contract/src/payment_contract_info.rs
@@ -8,21 +8,26 @@ use soroban_sdk::{contracttype, Address, Bytes};
 ///Struct that stores the necessary information for the contract
 pub struct PaymentContractInfo {
     /// The contract manager of the contract
-    pub contract_manager: Address,
+    pub contract_manager: ContractManager,
     /// The identification of the company in an off chain storage
     pub company_id: Bytes,
     /// The identification of the project in an off chain storage
     pub project_id: Bytes,
     /// The identification of the contract in an off chain storage
-    pub contract_id: Bytes,
+    pub contract_name: Bytes,
     /// The way the payment will be executed (only native (xlm) for now)
     pub payment_method: PaymentMethod,
     /// The payment amount for each approved asset
     pub asset_payment_amount: u64,
     /// Contract creation date
     pub creation_date: u64,
+    /// The accorded date to start the contract execution
+    pub start_date: u64,
     /// The last day on which assets could be uploaded
     pub deadline: u64,
+    /// Conditions of the contract
+    pub scope_of_work: Bytes,
+    pub rights_royalties: Bytes,
     pub payment_time: u64,
     pub contract_type: ContractType,
 }
@@ -40,4 +45,13 @@ pub enum ContractType {
     FixedPrice,
     Milestones,
     Licensing,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+#[contracttype]
+pub struct ContractManager {
+    address: Address,
+    name: Bytes,
+    title: Bytes,
+    physical_address: Bytes,
 }
